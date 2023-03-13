afd4fbbd825738abaf623c6fe63a9f7f708f3c87
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-03-13 00:45:12 -0400

    feat/added-char-cards-with-images

--- src/pages/api/characters.js
+++ src/pages/api/characters.js
@@ -9,12 +9,20 @@ Array.from({length:12}).forEach(()=>{
         animal: faker.animal.type(),
         age: faker.datatype.number({min:1, max:16}),
         name: faker.name.firstName(),
-        image: faker.image.imageUrl(),
+        personality: faker.word.adjective(),
+        health:faker.datatype.number({min:5, max:8}),
+        level: faker.datatype.number({min:1, max:5}),
+        occupation: faker.name.jobType(),
+        kryptonite: faker.science.chemicalElement(),
+        spells: faker.lorem.words(8),
+        image: faker.image.animals(),
         description: faker.lorem.paragraph()
 
     })
 })
 
-export default function handler(req, res) {
+const  handler = (req, res) => {
     res.status(200).json({fakeChars })
 }
+
+export default handler
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import Image from "next/image";
+
+const API_URL = 'http://localhost:3000'
 
 const navigation = [
     { name: 'Sign Up', href: '#', current: true },
@@ -13,6 +17,24 @@ function classNames(...classes) {
 }
 
 const Home = () => {
+
+    const [characters, setCharacters ] = useState([])
+
+    useEffect(()=>{
+        async function getCharacters() {
+            try {
+                const data = await axios.get(`${API_URL}/api/characters`)
+                setCharacters(data.data.fakeChars)
+                console.log(characters)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getCharacters()
+    }, [])
+
+
+
     return (
         <div className="w-full h-full flex flex-col">
              <header className="w-full h-32 my-4 flex items-center  bg-neutral-800 justify-end space-x-4 px-4" >
@@ -32,7 +54,34 @@ const Home = () => {
 
                      ))}
              </header>
-            <main>This is the main content</main>
+            <main>
+                <section className='my-4 p-4 grid grid-cols-3 gap-4 lg:grid-cols-4'>
+                    { characters && characters.length > 0 &&
+                        characters.map((elem) => (
+                       <div className='p-4 hover:scale-105 hover:border-black hover:shadow-black transition transform my-4 border border-blue-300 rounded'
+                           key={elem.id}
+                       >
+                           <div className='aspect-square relative mb-4'>
+                               <Image className='rounded-full'
+                                      src={elem.image+'?'+elem.id}
+                                      alt = {elem.name}
+                                      layout="fill"
+                                      objectFit="cover"
+                               ></Image>
+                           </div>
+                           <h2 className='text-xl font-semibold truncate'>{elem.name} the {elem.personality} {elem.animal}</h2>
+                           <p><em>Occupation: </em>{elem.occupation}</p>
+                           <p className='truncate'><em>Known Spells: </em>{elem.spells}</p>
+
+                       </div>
+                    ))
+                    }
+
+<hr />
+
+                {/*{JSON.stringify(characters)}*/}
+                    </section>
+            </main>
             <footer>This is the footer</footer>
 
         </div>
